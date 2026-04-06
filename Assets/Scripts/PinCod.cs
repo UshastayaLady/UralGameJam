097a9557a7a591d5deb4a701829a8c6e6cd4c11d
B3441 <1arya11stark@gmail.com>
2026-04-06 17:40:14 +0300

    Вроде добила полностью 1 сцену, на второй поставила головоломку 1

--- Assets/Scripts/PinCod.cs
+++ Assets/Scripts/PinCod.cs
@@ -1,24 +1,23 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PinCod : MonoBehaviour
 {
     [SerializeField] private string _pincode;   
-    [SerializeField] private GameObject activeObject;
+    [SerializeField] private GameObject _activeObject;
+    [SerializeField] private Text _text;
     private string _writePin;
 
-    protected void OnEnable()
-    {
-        AnswerClick.ClickedAnswer -= AddPin;
-    }
 
-    private void AddPin(int number)
+    public void AddPin(int number)
     {
         _writePin = _writePin + $"{number}";
+        _text.text = _writePin;
         if (_writePin.Length >= 4)
         {
             if (_pincode.Equals(_writePin))
             {
-                activeObject.SetActive(true);
+                _activeObject.SetActive(true);
                 enabled = false;
             }
             else 
@@ -26,10 +25,5 @@ public class PinCod : MonoBehaviour
                 _writePin = "";
             }
         }        
-    }
-
-    protected void OnDisable()
-    {
-        AnswerClick.ClickedAnswer -= AddPin;
     }
 }
